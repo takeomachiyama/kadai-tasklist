cf7e7a313980da6263d0f1727d74895b8ab4e9cd
enhance <aquarydance@yahoo.co.jp>
2017-08-05 11:25:14 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,12 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+#    @tasks = Task.all.page(params[:page]).per(3)
+    if (logged_in?)
+      @tasks = current_user.tasks.page(params[:page]).per(3)
+    else 
+      @tasks = []
+    end
   end
 
   def show
@@ -48,12 +53,16 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+    if (@task == nil)
+      redirect_to root_url
+    end
   end
   
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status, :user_id)
   end
 
 end
